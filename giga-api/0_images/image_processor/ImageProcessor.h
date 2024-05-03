2d12902a55e90aa01956eef0e0e4064c095c766f
Omeed Tehrani <61725820+omeedcs@users.noreply.github.com>
2024-05-03 18:24:41 -0500

    Update ImageProcessor.h

--- giga-api/0_images/image_processor/ImageProcessor.h
+++ giga-api/0_images/image_processor/ImageProcessor.h
@@ -25,20 +25,3 @@ private:
 };
 
 #endif
-
-#include "ImageProcessor.h"
-#include "MegaGPU.h"
-
-// int main() {
-//     std::string imagePath = "image.png";
-//     ImageProcessor processor;
-//     processor.prepareImage(imagePath);
-//     MegaGPU mega;
-//     int scaleFactor = 2;
-//     std::vector<unsigned char> input = processor.getInput();
-//     std::vector<unsigned char> output = processor.getOutput();
-//     mega.upsampleImage(input.data(), output.data(), processor.getWidth(), processor.getHeight(), scaleFactor);
-//     cv::Mat resultImg(processor.getHeight() * scaleFactor, processor.getWidth() * scaleFactor, CV_8UC3, output.data());
-//     cv::imwrite("upsampled_image.png", resultImg);
-//     return 0;
-// }
